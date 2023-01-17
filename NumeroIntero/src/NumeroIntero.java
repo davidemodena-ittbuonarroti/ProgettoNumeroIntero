e4676a3893d012fee58683a392262ca6efe0c67f
davidemodena-ittbuonarroti <davide.modena@buonarroti.tn.it>
2023-01-17 20:18:23 +0100

    fix: removed NumeroIntero constructor

--- NumeroIntero/src/NumeroIntero.java
+++ NumeroIntero/src/NumeroIntero.java
@@ -1,10 +1,6 @@
 public class NumeroIntero {
     private int numero;
-
-    public NumeroIntero(int numero){
-        this.numero = numero;
-    }
-
+    
     public int getNumero() {
         return numero;
     }
